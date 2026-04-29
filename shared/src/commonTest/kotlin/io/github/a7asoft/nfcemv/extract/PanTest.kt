2e14e490d004c31cfc91f80b80a014fddce0e45b
Adrian Perez Zaldivar <a7asoft@gmail.com>
2026-04-29 14:28:44 -0400

    test(extract): pin Pan rejection for length above 19 (#5)

--- shared/src/commonTest/kotlin/io/github/a7asoft/nfcemv/extract/PanTest.kt
+++ shared/src/commonTest/kotlin/io/github/a7asoft/nfcemv/extract/PanTest.kt
@@ -11,4 +11,10 @@ class PanTest {
         assertFailsWith<IllegalArgumentException> { Pan("0") }
         assertFailsWith<IllegalArgumentException> { Pan("") }
     }
+
+    @Test
+    fun `rejects strings longer than 19 digits with IllegalArgumentException`() {
+        assertFailsWith<IllegalArgumentException> { Pan("12345678901234567890") } // 20d
+        assertFailsWith<IllegalArgumentException> { Pan("0".repeat(25)) }
+    }
 }
