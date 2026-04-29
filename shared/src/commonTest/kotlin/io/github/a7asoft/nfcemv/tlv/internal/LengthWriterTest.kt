a9fec5d52c9ba63305d3d7d71dd06a9372219e18
Adrian Perez Zaldivar <a7asoft@gmail.com>
2026-04-29 00:24:22 -0400

    feat(tlv): writeLength short form (length 0) (#2)

---
+++ shared/src/commonTest/kotlin/io/github/a7asoft/nfcemv/tlv/internal/LengthWriterTest.kt
@@ -0,0 +1,21 @@
+package io.github.a7asoft.nfcemv.tlv.internal
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class LengthWriterTest {
+
+    @Test
+    fun `lengthOctets is 1 for length 0`() {
+        assertEquals(1, lengthOctets(0))
+    }
+
+    @Test
+    fun `writeLength encodes 0 as a single 0x00 byte`() {
+        val dst = ByteArray(1)
+        val end = writeLength(0, dst, 0)
+        assertEquals(1, end)
+        assertContentEquals(byteArrayOf(0x00), dst)
+    }
+}
